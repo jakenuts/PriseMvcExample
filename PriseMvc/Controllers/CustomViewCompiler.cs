ece179062b5d5aed95cbc1a1011e89cae4559e1f
James White <james@gbase.com>
2022-10-05 22:05:43 -0700

    Cleaned up project

--- PriseMvc/Controllers/CustomViewCompiler.cs
+++ PriseMvc/Controllers/CustomViewCompiler.cs
@@ -180,26 +180,3 @@ public partial class CustomViewCompiler : IViewCompiler
         }
     }
 }
-
-public class CustomViewCompilerProvider : IViewCompilerProvider
-{
-    private CustomViewCompiler _compiler;
-    private ApplicationPartManager _applicationPartManager;
-    private ILoggerFactory _loggerFactory;
-
-    public CustomViewCompilerProvider(
-        ApplicationPartManager applicationPartManager,
-        ILoggerFactory loggerFactory)
-    {
-        _applicationPartManager = applicationPartManager;
-        _loggerFactory = loggerFactory;
-        Refresh();
-    }
-
-    public void Refresh()
-    {
-        _compiler = new CustomViewCompiler(_applicationPartManager, _loggerFactory.CreateLogger<CustomViewCompiler>());
-    }
-
-    public IViewCompiler GetCompiler() => _compiler;
-}
---
+++ PriseMvc/Controllers/CustomViewCompilerProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+
+namespace PriseMvc.Controllers;
+
+public class CustomViewCompilerProvider : IViewCompilerProvider
+{
+    private CustomViewCompiler _compiler;
+    private ApplicationPartManager _applicationPartManager;
+    private ILoggerFactory _loggerFactory;
+
+    public CustomViewCompilerProvider(
+        ApplicationPartManager applicationPartManager,
+        ILoggerFactory loggerFactory)
+    {
+        _applicationPartManager = applicationPartManager;
+        _loggerFactory = loggerFactory;
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        _compiler = new CustomViewCompiler(_applicationPartManager, _loggerFactory.CreateLogger<CustomViewCompiler>());
+    }
+
+    public IViewCompiler GetCompiler() => _compiler;
+}
